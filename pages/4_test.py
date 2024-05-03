6d5fc02784510b5e6176325d3731a0f4ecf4bf45
sarthak-r <32743666+sarthak-r@users.noreply.github.com>
2024-05-03 12:13:49 +0530

    t

--- pages/4_test.py
+++ pages/4_test.py
@@ -9,6 +9,7 @@ if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
 
+
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
@@ -17,7 +18,7 @@ if prompt := st.chat_input():
         st.info("Please add your OpenAI API key to continue.")
         st.stop()
 
-    client = OpenAI(api_key=openai_api_key)
+    client = OpenAI(api_key='sk-f')
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
     response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
