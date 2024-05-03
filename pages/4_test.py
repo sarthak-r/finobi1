357f061e13d2889358a0951aeb5d5c65d280ca4b
sarthak-r <32743666+sarthak-r@users.noreply.github.com>
2024-05-03 12:15:35 +0530

    d

--- pages/4_test.py
+++ pages/4_test.py
@@ -1,14 +1,13 @@
 from openai import OpenAI
 import streamlit as st
 
-
-
+with st.sidebar:
+    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+    
 st.title("💬 Chatbot")
 st.caption("🚀 A streamlit chatbot powered by OpenAI LLM")
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
-
-
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
@@ -18,7 +17,7 @@ if prompt := st.chat_input():
         st.info("Please add your OpenAI API key to continue.")
         st.stop()
 
-    client = OpenAI(api_key='sk-fHV8qTEEbXEPrK8NKolvT3BlbkFJmB9NJ4nKadi5jpZ8REkPs')
+    client = OpenAI(api_key=openai_api_key)
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
     response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
