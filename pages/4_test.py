c3cf82a1d6181d0430ada11ca0a158cbfae79d72
sarthak-r <32743666+sarthak-r@users.noreply.github.com>
2024-05-03 12:11:09 +0530

    t

--- pages/4_test.py
+++ pages/4_test.py
@@ -1,2 +1,9 @@
 from openai import OpenAI
 import streamlit as st
+
+
+
+st.title("💬 Chatbot")
+st.caption("🚀 A streamlit chatbot powered by OpenAI LLM")
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
