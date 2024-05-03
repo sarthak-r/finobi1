cfa775b9116f0e44eeb9210e86145f9e9d31608b
sarthak-r <32743666+sarthak-r@users.noreply.github.com>
2024-05-03 12:14:16 +0530

    t

--- pages/4_test.py
+++ pages/4_test.py
@@ -18,7 +18,7 @@ if prompt := st.chat_input():
         st.info("Please add your OpenAI API key to continue.")
         st.stop()
 
-    client = OpenAI(api_key='sk-ffsdkjfghfs')
+    client = OpenAI(api_key='sk-fHV8qTEEbXEPrK8NKolvT3BlbkFJmB9NJ4nKadi5jpZ8REkPs')
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
     response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
