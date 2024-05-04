d27d4f3a9c41561a8306fe5dd058e5610212918a
sarthak-r <32743666+sarthak-r@users.noreply.github.com>
2024-05-04 10:17:31 +0530

    login

---
+++ pages/5_login.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import streamlit_authenticator as stauth
+
+import yaml
+from yaml.loader import SafeLoader
+
+with open('../config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['pre-authorized']
+)
+
+
+authenticator.login()
