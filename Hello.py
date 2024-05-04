accfbc4ca39711b0e8ddb5ab83efa28c080c1997
sarthak-r <32743666+sarthak-r@users.noreply.github.com>
2024-05-04 11:14:09 +0530

    some changes

--- Hello.py
+++ Hello.py
@@ -1,47 +1,27 @@
-import altair as alt
-import numpy as np
-import pandas as pd
 import streamlit as st
+import streamlit_authenticator as stauth
 
+import yaml
+from yaml.loader import SafeLoader
 
-# Function to calculate balance at each year
-def calculate_balance(current_age, super_bal, annual_contribution, retirement_age, roi, inflation_rate, income_replacement_ratio, life_expectancy):
-    years = np.arange(current_age, life_expectancy + 1)
-    balance = np.zeros(len(years))
-    balance[0] = super_bal
-    annual_expenses = super_bal * (income_replacement_ratio / 100)
-    for i in range(1, len(years)):
-        balance[i] = (balance[i - 1] * (1 + roi / 100) + annual_contribution) / (1 + inflation_rate / 100)
-        if i >= retirement_age - current_age:
-            balance[i] -= annual_expenses
-    return years, balance
+with open("./config.yaml","r") as file:
+    config = yaml.load(file, Loader=SafeLoader)
 
-# Streamlit app
-st.title("Retirement Cashflow Modelmm")
-
-current_age = st.slider("Current age", 20, 80, 30)
-super_bal = st.slider("Current super balance", 100, 1000000, 250000)
-annual_contribution = st.slider("Annual contribution to super", 0, 50000, 10000)
-retirement_age = st.slider("Retirement age", current_age + 1, 80, 60)
-roi = st.slider("Return percentage", 0, 25, 7)
-inflation_rate = st.slider("Inflation rate", 0, 10, 2)
-income_replacement_ratio = st.slider("Income replacement ratio (%)", 50, 150, 70)
-life_expectancy = st.slider("Life expectancy", 80, 100, 85)
-
-years, balance = calculate_balance(current_age, super_bal, annual_contribution, retirement_age, roi, inflation_rate, income_replacement_ratio, life_expectancy)
-df = pd.DataFrame({"Year": years, "Balance": balance})
-
-st.write("### Cashflow Model")
-
-# Bar chart with interactive tooltip
-tooltip = [alt.Tooltip("Year:O", title="Year"), alt.Tooltip("Balance:Q", title="Balance", format=".2f")]
-chart = alt.Chart(df).mark_bar().encode(
-    x="Year:O",
-    y="Balance:Q",
-    tooltip=tooltip
-).properties(
-    width=700,
-    height=400
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['pre-authorized']
 )
 
-st.altair_chart(chart, use_container_width=True)
+authenticator.login()
+
+if st.session_state["authentication_status"]:
+    authenticator.logout()
+    st.write(f'Welcome *{st.session_state["name"]}*')
+    st.title('Some content')
+elif st.session_state["authentication_status"] is False:
+    st.error('Username/password is incorrect')
+elif st.session_state["authentication_status"] is None:
+    st.warning('Please enter your username and password')
