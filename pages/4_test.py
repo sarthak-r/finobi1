f4148146155bbb7575ae851d14f50a16f926a375
sarthak-r <32743666+sarthak-r@users.noreply.github.com>
2024-05-04 14:49:43 +0530

    test

--- pages/4_test.py
+++ pages/4_test.py
@@ -3,6 +3,16 @@ import streamlit as st
 import streamlit_authenticator as stauth
 import os
 
+
+from enum import Enum
+from typing import Set
+
+from pydantic import BaseModel, Field, ValidationError, parse_obj_as
+
+import streamlit_pydantic as sp
+
+
+
 with open("./config.yaml","r") as file:
     config = yaml.load(file, Loader=SafeLoader)
 
@@ -45,3 +55,59 @@ if prompt := st.chat_input():
 
 st.title(os.getcwd())
 st.caption("hello")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class OtherData(BaseModel):
+    text: str
+    integer: int
+
+
+class SelectionValue(str, Enum):
+    FOO = "foo"
+    BAR = "bar"
+
+
+class ExampleModel(BaseModel):
+    long_text: str = Field(
+        ..., format="multi-line", description="Unlimited text property"
+    )
+    integer_in_range: int = Field(
+        20,
+        ge=10,
+        le=30,
+        multiple_of=2,
+        description="Number property with a limited range.",
+    )
+    single_selection: SelectionValue = Field(
+        ..., description="Only select a single item from a set."
+    )
+    multi_selection: Set[SelectionValue] = Field(
+        ..., description="Allows multiple items from a set."
+    )
+    read_only_text: str = Field(
+        "Lorem ipsum dolor sit amet",
+        description="This is a ready only text.",
+        readOnly=True,
+    )
+    single_object: OtherData = Field(
+        ...,
+        description="Another object embedded into this model.",
+    )
+
+
+data = sp.pydantic_form(key="my_form", model=ExampleModel)
+if data:
+    st.json(data.json())
