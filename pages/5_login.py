59b058c352c6186370bcce950e44f9e521f8d9c0
sarthak-r <32743666+sarthak-r@users.noreply.github.com>
2024-05-04 11:02:41 +0530

    lgin

--- pages/5_login.py
+++ pages/5_login.py
@@ -15,7 +15,14 @@ authenticator = stauth.Authenticate(
     config['pre-authorized']
 )
 
-name, authentication_status, username = authenticator.login('Login', 'main')
-
 
 authenticator.login()
+
+if st.session_state["authentication_status"]:
+    authenticator.logout()
+    st.write(f'Welcome *{st.session_state["name"]}*')
+    st.title('Some content')
+elif st.session_state["authentication_status"] is False:
+    st.error('Username/password is incorrect')
+elif st.session_state["authentication_status"] is None:
+    st.warning('Please enter your username and password')
